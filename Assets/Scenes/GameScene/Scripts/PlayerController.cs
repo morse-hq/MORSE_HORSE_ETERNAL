11828b9ad37737b318a12d7d7d54ebe7ef263d3b
bobby45800 <bobby45900@gmail.com>
2020-08-07 20:14:47 +0100

    Player controller script

---
+++ Assets/Scenes/GameScene/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField, Tooltip("Max speed, in units per second, that the character moves.")]
+    float speed = 9;
+
+    [SerializeField, Tooltip("Acceleration while grounded.")]
+    float walkAcceleration = 75;
+
+    [SerializeField, Tooltip("Acceleration while in the air.")]
+    float airAcceleration = 30;
+
+    [SerializeField, Tooltip("Deceleration applied when character is grounded and not attempting to move.")]
+    float groundDeceleration = 70;
+
+    [SerializeField, Tooltip("Max height the character will jump regardless of gravity")]
+    float jumpHeight = 4;
+
+    private BoxCollider2D boxCollider;
+
+    private Vector2 velocity;
+
+    private bool grounded;
+
+    void Start() {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update() {
+        // Jumping
+        if (grounded) {
+            velocity.y = 0;
+            if (Input.GetButtonDown("Jump")) {
+                // Calculate the velocity required to achieve the target jump height
+                velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+            }
+        }
+
+        float acceleration = grounded ? walkAcceleration : airAcceleration;
+        float deceleration = grounded ? groundDeceleration : 0;
+
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        if (moveInput != 0) {
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, acceleration * Time.deltaTime);
+        }
+        else {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+        }
+
+        velocity.y += Physics2D.gravity.y * Time.deltaTime;
+        transform.Translate(velocity * Time.deltaTime);
+        grounded = false;
+    }
+}
