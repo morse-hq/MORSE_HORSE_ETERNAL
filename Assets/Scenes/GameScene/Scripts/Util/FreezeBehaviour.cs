10c9e640dd4eba47bef7b722eedab0e91b78a96a
DaMan56 <43971727+DaMan56100@users.noreply.github.com>
2020-08-08 16:59:36 +0100

    meta yeta

---
+++ Assets/Scenes/GameScene/Scripts/Util/FreezeBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeBehaviour : MonoBehaviour
+{
+    public bool grayscaleOnPause = false;
+
+    public Material normalMaterial;
+    public Material grayscaleMaterial;
+
+    private Renderer renderer;
+    private TimeStop timeStop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        timeStop = ScriptableObject.CreateInstance<TimeStop>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grayscaleOnPause && renderer != null)
+        {
+            // change material to grayscale if time is stopped
+            renderer.material = timeStop.getIsTimeStopped() ? grayscaleMaterial : normalMaterial;
+        }
+    }
+}
