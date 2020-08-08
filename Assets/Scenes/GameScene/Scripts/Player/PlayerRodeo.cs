32d168a1bedf779356c7aba5c2f33b27c0035f4d
DaMan56 <43971727+DaMan56100@users.noreply.github.com>
2020-08-08 21:45:38 +0100

    Fixed this script beatboxing out errors

--- Assets/Scenes/GameScene/Scripts/Player/PlayerRodeo.cs
+++ Assets/Scenes/GameScene/Scripts/Player/PlayerRodeo.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class PlayerRodeo : MonoBehaviour
 {
     private GameObject ridingObject;
+    private Rigidbody2D myRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ridingObject != null && ridingObject.GetComponent<Rigidbody2D>() != null)
+        if (ridingObject != null && ridingObjectRigidBody() != null)
         {
             Debug.Log(ridingObject);
             transform.position += (Vector3)ridingObject.GetComponent<Rigidbody2D>().velocity * Time.deltaTime;
@@ -29,6 +30,22 @@ public class PlayerRodeo : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (ridingObject != null && ridingObjectRigidBody() != null)
+        {
+            // transfer x momentum across 
+            myRigidbody.velocity = new Vector2(
+                ridingObjectRigidBody().velocity.x,
+                myRigidbody.velocity.y
+            );
+        }
+
         ridingObject = null;
+    }
+
+    private Rigidbody2D ridingObjectRigidBody()
+    {
+        Rigidbody2D foundRigidBody = null;
+        ridingObject.TryGetComponent<Rigidbody2D>(out foundRigidBody);
+        return foundRigidBody;
     }
 }
