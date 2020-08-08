4f8319720ce5006be3ecfdf1a5f9337ec9036e42
DaMan56 <43971727+DaMan56100@users.noreply.github.com>
2020-08-08 21:15:52 +0100

    Added Rodeo script
    You can now ride bullets

---
+++ Assets/Scenes/GameScene/Scripts/Player/PlayerRodeo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRodeo : MonoBehaviour
+{
+    private GameObject ridingObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ridingObject != null && ridingObject.GetComponent<Rigidbody2D>() != null)
+        {
+            Debug.Log(ridingObject);
+            transform.position += (Vector3)ridingObject.GetComponent<Rigidbody2D>().velocity * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ridingObject = collision.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ridingObject = null;
+    }
+}
