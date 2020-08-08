8401a9428649d633e33f5fa3bbc03159014ef937
DaMan56 <43971727+DaMan56100@users.noreply.github.com>
2020-08-08 14:58:56 +0100

    Laser beam now has particles.
    And I added it, if I didn't already push

--- Assets/Scenes/GameScene/Scripts/Laser/LaserBeam.cs
+++ Assets/Scenes/GameScene/Scripts/Laser/LaserBeam.cs
@@ -5,29 +5,30 @@ using UnityEngine;
 public class LaserBeam : MonoBehaviour
 {
     LineRenderer lineRenderer;
+    ParticleSystem particleSystem;
 
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = transform.GetComponentInChildren<LineRenderer>();
-        Vector3[] tempVectors = new Vector3[lineRenderer.positionCount];
-        lineRenderer.GetPositions(tempVectors);
-        foreach (Vector3 vector in tempVectors)
-        {
-            Debug.Log(vector);
-        }
-        lineRenderer.SetPositions(new Vector3[2]); // initilise with two empty point
+        lineRenderer.positionCount = 2;
         lineRenderer.SetPosition(0, new Vector3(0,0,1)); // first point is my position
+
+        particleSystem = transform.GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 100f);
-        lineRenderer.SetPosition(1, transform.position - (Vector3)hit.point);
-        Debug.Log("START");
-        Debug.Log(hit.point);
-        Debug.Log(transform.position);
-        Debug.Log(transform.position - (Vector3)hit.point);
+        Vector3 relativeHitPosition = transform.position - (Vector3)hit.point;
+        lineRenderer.SetPosition(1, relativeHitPosition);
+
+        particleSystem.transform.position = hit.point;
+
+        if (hit.collider.gameObject.name.Equals("Player"))
+        {
+            hit.collider.gameObject.GetComponent<PlayerKiller>().killPlayer();
+        }
     }
 }
