67b992e74accc5411876264bc9b7dbe43f14c3ae
DaMan56 <43971727+DaMan56100@users.noreply.github.com>
2020-08-08 16:09:59 +0100

    Camera now locks to the player

---
+++ Assets/Scenes/GameScene/Scripts/Camera/CameraLockToPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLockToPlayer : MonoBehaviour
+{
+    GameObject playerGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerGameObject = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3
+            (
+                playerGameObject.transform.position.x,
+                playerGameObject.transform.position.y,
+                -10
+            );
+    }
+}
