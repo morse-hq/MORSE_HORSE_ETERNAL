bafb42ddbb4a8e142d2fb9afd0dfdc7afb53f8af
DaMan56 <43971727+DaMan56100@users.noreply.github.com>
2020-08-08 14:58:33 +0100

    Added PlayerKiller to player
    It'll handle the player dying from multiple locations

---
+++ Assets/Scenes/GameScene/Scripts/Player/PlayerKiller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKiller : MonoBehaviour
+{
+    public void killPlayer()
+    {
+        Destroy(this.gameObject);
+    }
+}
