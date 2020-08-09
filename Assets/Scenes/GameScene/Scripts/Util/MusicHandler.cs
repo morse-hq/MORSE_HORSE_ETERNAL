1efe6ee6d1812984acb41b529227349dd386bd87
bobby45800 <bobby45900@gmail.com>
2020-08-09 10:11:57 +0100

    dd

--- Assets/Scenes/GameScene/Scripts/Util/MusicHandler.cs
+++ Assets/Scenes/GameScene/Scripts/Util/MusicHandler.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 public class MusicHandler : MonoBehaviour
 {
     private AudioSource unpausedMusic;
-    private AudioSource pausedMusic;
 
     // Start is called before the first frame update
     void Start() {
-        // GameObject.FindGameObjectWithTag("UnpausedMusic").GetComponent<MusicHandler>().PlayMusic();
+        GameObject.FindGameObjectWithTag("UnpausedMusic").GetComponent<MusicHandler>().PlayMusic();
     }
 
     private void Awake() {
         DontDestroyOnLoad(transform.gameObject);
         unpausedMusic = GetComponent<AudioSource>();
-        pausedMusic = GetComponent<AudioSource>();
     }
 
     public void PlayPausedMusic() {
---
+++ Assets/Scenes/MenuScene/Scripts/MusicHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicHandler : MonoBehaviour {
+    private AudioSource menuMusic;
+
+    // Start is called before the first frame update
+    void Start() {
+        GameObject.FindGameObjectWithTag("MenuMusic").GetComponent<MusicHandler>().PlayMusic();
+    }
+
+    private void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+        menuMusic = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic() {
+        if (menuMusic.isPlaying) return;
+        menuMusic.Play();
+    }
+
+    public void StopMusic() {
+        menuMusic.Stop();
+    }
+}
