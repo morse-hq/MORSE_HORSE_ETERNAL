d02ed2b655467d94534a22daf16e435ab237684c
DaMan56 <43971727+DaMan56100@users.noreply.github.com>
2020-08-09 10:06:03 +0100

    fixed gamescene?

--- Assets/Scenes/GameScene/Scripts/Background/BackgroundParallax.cs
+++ Assets/Scenes/GameScene/Scripts/Background/BackgroundParallax.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class BackgroundParallax : MonoBehaviour
 {
+    public int parallaxPercent = 50;
+
+    public int someotherInt;
+
+    private GameObject playerGameObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerGameObject = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerGameObject != null)
+        {
+            float parallax = (float)parallaxPercent / 100;
+
+            Debug.Log(parallax);
+
+            transform.position = playerGameObject.transform.position * parallax;
+        }
     }
 }
